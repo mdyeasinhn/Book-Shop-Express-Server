74919e8b05c5bba3f997b01c818fea480fa02c3f
Md Yeasin <mdyeasinhn@gmail.com>
2024-12-11 23:16:30 +0600

    initial setup mongodb connectd app is running

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import userRouter from "./module/user/product.route";
+const app: Application = express();
+const port = 3000;
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/user', userRouter)
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    status: true,
+    message: "server Live",
+  });
+});
+
+console.log(process.cwd());
+
+export default app;
